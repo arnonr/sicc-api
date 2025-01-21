b9f19735f6864575d4b7b651b306fbf382b175e4
arnonr <arnonr@kmutnb.ac.th>
2025-01-21 10:16:07 +0700

    up

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(fileUpload());
-app.use("/static", express.static(__dirname + "/public"));
+app.use("/sicc-static", express.static(__dirname + "/public"));
 
 app.use(routes);
 
